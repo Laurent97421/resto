6709b6df926a8a7b33df7d90059fa50954d937cd
LTECHER <laurent.techer@ynov.com>
2022-08-01 12:53:40 +0200

    ajout des overlay dans la homescreen/ route sign-in en back

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// Sign In
+router.post('/sign-in', async function(req, res, next) {
+
+  var error = [];
+
+  if(req.body.emailFromFront == '' || req.body.passwordFromFront == '') {
+    error.push('Un ou plusieurs champ(s) sont vide(s)')
+  } else {
+    var userFromFrontExist = await userModel.findOne({email: req.body.emailFromFront});
+    var result = false;
+    if(userFromFrontExist){
+      result = true
+    } else {
+      error.push('Information(s) incorrecte(s)');
+    }
+  }
+
+  res.json({result, userFromFrontExist, error})
+})
 module.exports = router;
