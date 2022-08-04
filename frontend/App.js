7dca2ff033c6553aa0d26c95a9fb3af7003766d2
Johann Darbas <johanndarbas@yahoo.com>
2022-08-04 18:14:27 +0200

    resultsPage

--- frontend/App.js
+++ frontend/App.js
@@ -1,65 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "react-native-gesture-handler";
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import HomeScreen from './screens/HomeScreen';
-import ResultScreen from './screens/ResultScreen';
-import RestoScreen from './screens/RestoScreen';
+import HomeScreen from "./screens/HomeScreen";
+import ResultScreen from "./screens/ResultScreen";
+import RestoScreen from "./screens/RestoScreen";
 
-import MyReservationScreen from './screens/HistoScreen';
-import AccountScreen from './screens/AccountScreen';
-import ReservationScreen from './screens/ReservationScreen';
+import MyReservationScreen from "./screens/HistoScreen";
+import AccountScreen from "./screens/AccountScreen";
+import ReservationScreen from "./screens/ReservationScreen";
 
-import { Provider } from 'react-redux';
-import { combineReducers }  from 'redux';
-import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from "react-redux";
+import { combineReducers } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-import token from './reducers/token'
-
+import token from "./reducers/token";
 
 // import { LogBox } from 'react-native';
 // LogBox.ignoreLogs(['Warning: ...']);
 
-const reducer = combineReducers({ token })
-const store = configureStore({reducer})
+const reducer = combineReducers({ token });
+const store = configureStore({ reducer });
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
-const StackNavigatorTest = function() { // On sauvegarde une fonction StackNavigatorTest dans une const
+const StackNavigatorTest = function () {
+  // On sauvegarde une fonction StackNavigatorTest dans une const
   return (
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Result" component={ResultScreen} />
-        <Stack.Screen name="Resto" component={RestoScreen} />
-        <Stack.Screen name="Reservation" component={ReservationScreen} />
-      </Stack.Navigator> 
-  )
-}
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Result" component={ResultScreen} />
+      <Stack.Screen name="Resto" component={RestoScreen} />
+      <Stack.Screen name="Reservation" component={ReservationScreen} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Tab.Navigator initialRouteName='Restaurant'>
+        <Tab.Navigator initialRouteName="Restaurant">
           <Tab.Screen name="Mon compte" component={AccountScreen} />
           <Tab.Screen name="Restaurant" component={StackNavigatorTest} />
           <Tab.Screen name="Mes réservations" component={MyReservationScreen} />
         </Tab.Navigator>
       </NavigationContainer>
     </Provider>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- frontend/screens/ReservationScreen.js
+++ frontend/screens/ReservationScreen.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { StyleSheet, View, Text, ScrollView } from "react-native";
 
-
-
-    
 export default function ReservationScreen() {
-
   return (
     <ScrollView>
-        <Text h4 style={{textAlign: 'center'}}>RESERVATIONSCREEN</Text>
+      <Text h4 style={{ textAlign: "center" }}>
+        RESERVATIONSCREEN
+      </Text>
     </ScrollView>
- 
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
