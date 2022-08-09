b31be2b82c47ca1391f80aa50c18a265c3fafb1a
LTECHER <laurent.techer@ynov.com>
2022-08-09 15:05:13 +0200

    remove console.log from search.js

--- frontend/reducers/search.js
+++ frontend/reducers/search.js
@@ -1,9 +1,9 @@
 export default function (search=[], action) {
     if (action.type === 'saveSearchResto') {
-        var searchCopy = [...search,{adresse: action.adresse, date: action.date, heure: action.heure}, {a: 'KLFLEFE'}];
+        var searchCopy = {...search, adresse: action.adresse, date: action.date, heure: action.heure};
 
         // console.log(JSON.stringify(searchCopy))
-        console.log(searchCopy)
+        // console.log(searchCopy)
 
 
         return searchCopy;
