40288863aeb42926f5b8721168e8852a2b337698
LTECHER <laurent.techer@ynov.com>
2022-08-10 14:35:30 +0200

    modification du time picker

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -3,16 +3,14 @@ import { StyleSheet, View, Text, ScrollView, TouchableOpacity, TextInput } from 
 import { Button, Overlay, Input } from "@rneui/themed";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 import { LocaleConfig } from "react-native-calendars";
-import DateTimePicker from "@react-native-community/datetimepicker";
-import DatePicker from "@react-native-community/datetimepicker";
+// import DateTimePicker from "@react-native-community/datetimepicker";
+// import DatePicker from "@react-native-community/datetimepicker";
 import Authentification from "../Components/HomeScreen/Auth.overlays";
 import filters from "../assets/files-JSON/filters.json"
 import { acc } from "react-native-reanimated";
 import { connect } from "react-redux";
-// import { IconFill, IconOutline } from "@ant-design/icons-react-native";
-// import { Icon } from "@rneui/themed";
-// import { checkcircleo } from "react-native-vector-icons/AntDesign";
 import Icon from 'react-native-vector-icons/AntDesign';
+import DatePicker, {getFormatedDate} from 'react-native-modern-datepicker'
 
 
 
@@ -20,6 +18,7 @@ import Icon from 'react-native-vector-icons/AntDesign';
 function HomeScreen(props) {
 
 
+  // SET UP DU CALENDRIER //
   
   LocaleConfig.locales["fr"] = {
     monthNames: [
@@ -63,15 +62,14 @@ function HomeScreen(props) {
     today: "Aujourd'hui",
   };
   LocaleConfig.defaultLocale = "fr";
+  // // // // // // // //
 
 
   // Pour la recherche du restaurant
   const [searchAddressResto, setSearchAddressResto] = useState("");
   const [calendarVisible, setCalendarVisible] = useState(false);
   const [dateInfos, setDateInfos] = useState();
-  const [timeVisible, setTimeVisible] = useState(false);
   const [filter, setFilter] = useState([])
-// console.log('OHOZHRIOEHROIZHEIORHOIERH')
   
   // Selection des filtres  
   const listFilter = (indice, element) => {
@@ -85,20 +83,12 @@ function HomeScreen(props) {
       setColor("lightblue")
       setLogoColor("blue")
       setFilter([...filter, {element: element}])
-      // console.log(filter[0].element)
       if(active === true){
         setActive(false)
         setColor("lightgrey")
         setLogoColor("black")
-        // filter.map((data, i) => {
-          // console.log(data.element)
-          // console.log(data)
         var allFilter = filter.filter(e => e.element !== element)
         setFilter(allFilter)
-        // })
-        // setFilter(filter[0].filter(e => e.element !== element))
-        // moviesWishList.filter(e => e.movieName !== movieName)
-        // setFilter([])
       }
     };
 
@@ -136,20 +126,7 @@ function HomeScreen(props) {
     )
   }
 
-  const [timePicker, setTimePicker] = useState(false);
-  const [time, setTime] = useState(new Date(Date.now()));
-
-  const showTimePicker = () => {
-    setTimePicker(true);
-  };
-
-  const onTimeSelected = (event, value) => {
-    setTime(value);
-    setTimePicker(false);
-    setTimeVisible(false);
-  };
-
-
+ 
   // const searchResto = async () => {
   //   console.log("Search Resto");
   //   let privateAdressIP = "172.20.10.8";
@@ -164,221 +141,220 @@ function HomeScreen(props) {
   //   });
   // }
 
+  // SELECTION DE L'HEURE //
+  const [heureVisible, setHeureVisible] = useState(false);
 
+  const showHour = () => {
+    setHeureVisible(!heureVisible)
+  }
+  const [time, setTime] = useState('');
+  
 
-
+  const timePicker = ()=> {
     
-
-
+    return (
+      <DatePicker
+      mode="time"
+      minuteInterval={3}
+      onTimeChange={selectedTime => {setTime(selectedTime); showHour()}}
+    />
+    )
+  }
+  // // // // // // // //
 
   return (
     <ScrollView>
-      <Text h4 style={{textAlign: 'center'}}>Rechercher un restaurant</Text>
+    <Text h4 style={{textAlign: 'center'}}>Rechercher un restaurant</Text>
 
-      {/* Authentification Overlays */}
-      <Authentification/>
+    {/* Authentification Overlays */}
+    <Authentification/>
 
-      {/* SEARCH INPUTS */}
-      <View style = {styles.viewSearch}>
-          <TextInput 
-          style = {{margin: 12, borderWidth: 1, width: '30%', borderRadius: 5}}
-          placeholder="Adresse"
-          onChangeText={(msg) => setSearchAddressResto(msg)}
-          value = {searchAddressResto}
-          />
-
-          <TextInput 
-          onPressIn={() => {console.log('woula'); setCalendarVisible(true)}}
-          editable = {false}
-          style = {{margin: 12, borderWidth: 1, width: '30%', borderRadius: 5}}
-          placeholder="Date"
-          onChangeText={(msg) => setSearchAddressResto(msg)}
-          value = {dateInfos}
-          />
-
-          {!timePicker && (
-            <TextInput 
-              style = {{margin: 12, borderWidth: 1, width: '30%', borderRadius: 5}}
-              onPressIn = {() => {console.log('zebi'); setTimeVisible(); showTimePicker()}}
-              editable = {false}
-              placeholder="Heure"
-              onChangeText={(msg) => setTime(msg)}
-              value = {time.getHours().toString() + ':' + time.getMinutes().toString()}
-          />)}
-      </View>
-          
-        {/* CALENDAR OVERLAY */}
-        <Overlay isVisible={calendarVisible} overlayStyle={{width: '90%'}}>
-          <Calendar
-           onDayPress={day => {
-            // console.log('Selected day')
-            // console.log(day);
-            setDateInfos(day.dateString);
-            setCalendarVisible(false);
-          }}
-          // onMonthChange={month => {
-          //   console.log('month changed', month);
-          // }}
+    {/* SEARCH INPUTS */}
+    <View style = {styles.viewSearch}>
+        <TextInput 
+        style = {{margin: 12, borderWidth: 1, width: '30%', borderRadius: 5}}
+        placeholder="Adresse"
+        onChangeText={(msg) => setSearchAddressResto(msg)}
+        value = {searchAddressResto}
         />
+
+        <TextInput 
+        onPressIn={() => {console.log('woula'); setCalendarVisible(true)}}
+        editable = {false}
+        style = {{margin: 12, borderWidth: 1, width: '30%', borderRadius: 5}}
+        placeholder="Date"
+        onChangeText={(msg) => setSearchAddressResto(msg)}
+        value = {dateInfos}
+        />
+
+        <TextInput 
+            style = {{margin: 12, borderWidth: 1, width: '30%', borderRadius: 5}}
+            onPressIn = {() => {showHour()}}
+            editable = {false}
+            placeholder="Heure"
+            onChangeText={(msg) => setTime(msg)}
+            value = {time}
+        />
+        </View>
+        
+      {/* CALENDAR OVERLAY */}
+      <Overlay isVisible={calendarVisible} overlayStyle={{width: '90%'}}>
+        <Calendar
+         onDayPress={day => {
+          setDateInfos(day.dateString);
+          setCalendarVisible(false);
+        }}
+      />
+    </Overlay>
+
+        {/* TIME OVERLAY */}
+      <Overlay isVisible = {heureVisible} onBackdropPress = {showHour} overlayStyle = {{width: '90%', height: 'auto', borderRadius: 20}} >
+        {timePicker()}
       </Overlay>
 
 
-        {/* TIME OVERLAY */}
-        {timePicker && (
-          <Overlay overlayStyle = {{width: '23%'}}>
-            <DateTimePicker
-            value = {time}
-            mode = {'time'}
-            minuteInterval = {15}
-            display = {Platform.OS === 'ios' ? 'default' : 'default'}
-            is24hour = {false}
-            onChange = {onTimeSelected}
-            />
-          </Overlay>
-        )}
+    {/* FILTERS */}
 
+      {/* ALIMENTATION FILTERS */}
+      {
+        filters && filters.map((data) => {
 
-      {/* FILTERS */}
-
-        {/* ALIMENTATION FILTERS */}
-        {
-          filters && filters.map((data) => {
-
-            if(data.id == 1){
-              return(
-                // touchable()
-                <View key = {data.id}>
-                <View style={styles.filtreContainer}>
-                  {/* HEADER */}
-                  <Text style={styles.title}>{data.categoryName}</Text>
-                  {/* FILTERS */}
-                  <View style={styles.filtreCarre}>
-                    {data.filtres && data.filtres.map((alimentation, i) => {
-                      return(
-                        listFilter(i, alimentation.name)
-                      )
-                    })}
-                  </View>
+          if(data.id == 1){
+            return(
+              // touchable()
+              <View key = {data.id}>
+              <View style={styles.filtreContainer}>
+                {/* HEADER */}
+                <Text style={styles.title}>{data.categoryName}</Text>
+                {/* FILTERS */}
+                <View style={styles.filtreCarre}>
+                  {data.filtres && data.filtres.map((alimentation, i) => {
+                    return(
+                      listFilter(i, alimentation.name)
+                    )
+                  })}
                 </View>
               </View>
-              )
-            }
-          })
-        }
+            </View>
+            )
+          }
+        })
+      }
 
-        {/* ALLERGENES */}
-        {
-          filters && filters.map((data) => {
-            if(data.id == 2) {
-              return(
+      {/* ALLERGENES */}
+      {
+        filters && filters.map((data) => {
+          if(data.id == 2) {
+            return(
+            <View key = {data.id}>
+              <View style={styles.filtreContainer}>
+                {/* HEADER */}
+                <Text style={styles.title}>{data.categoryName}</Text>
+                {/* FILTERS */}
+                <View style={styles.filtreCarre}>
+                  {data.filtres && data.filtres.map((equipement, i) => {
+
+                    return(
+                      listFilter(i, equipement.name)
+                    )
+                  })}
+                </View>
+              </View>
+            </View>
+            )
+          }
+        })
+      }
+
+      {/* MOYENS DE PAIEMENT */}
+      {
+        filters && filters.map((data) => {
+          if(data.id == 3) {
+            return(
+            <View key = {data.id}>
+              <View style={styles.filtreContainer}>
+                {/* HEADER */}
+                <Text style={styles.title}>{data.categoryName}</Text>
+                {/* FILTERS */}
+                <View style={styles.filtreCarre}>
+                  {data.filtres && data.filtres.map((equipement, i) => {
+
+                    return(
+                      listFilter(i, equipement.name)
+                    )
+                  })}
+                </View>
+              </View>
+            </View>
+            )
+          }
+        })
+      }
+
+      {/* EQUIPEMENTS FILTERS */}
+      {
+        filters && filters.map((data) => {
+          if(data.id == 4) {
+            return(
+            <View key = {data.id}>
+              <View style={styles.filtreContainer}>
+                {/* HEADER */}
+                <Text style={styles.title}>{data.categoryName}</Text>
+                {/* FILTERS */}
+                <View style={styles.filtreCarre}>
+                  {data.filtres && data.filtres.map((equipement, i) => {
+
+                    return(
+                      listFilter(i, equipement.name)
+                      
+                    )
+                  })}
+                </View>
+              </View>
+            </View>
+            )
+          }
+        })
+      }
+
+      {/* ACCESS FILTERS */}
+      {
+        filters && filters.map((data) => {
+          if(data.id == 5){
+            return(
               <View key = {data.id}>
                 <View style={styles.filtreContainer}>
                   {/* HEADER */}
                   <Text style={styles.title}>{data.categoryName}</Text>
                   {/* FILTERS */}
                   <View style={styles.filtreCarre}>
-                    {data.filtres && data.filtres.map((equipement, i) => {
-
+                    {data.filtres && data.filtres.map((accessibilite, i) => {
                       return(
-                        listFilter(i, equipement.name)
+                        listFilter(i, accessibilite.name)
                       )
                     })}
                   </View>
                 </View>
               </View>
               )
-            }
-          })
-        }
+          }
+        })
+      }
 
-        {/* MOYENS DE PAIEMENT */}
-        {
-          filters && filters.map((data) => {
-            if(data.id == 3) {
-              return(
-              <View key = {data.id}>
-                <View style={styles.filtreContainer}>
-                  {/* HEADER */}
-                  <Text style={styles.title}>{data.categoryName}</Text>
-                  {/* FILTERS */}
-                  <View style={styles.filtreCarre}>
-                    {data.filtres && data.filtres.map((equipement, i) => {
+    {/* BUTTON SEARCH */}
+    <Button
+      style={{ justifyContent: "flex-end" }}
+      title="Rechercher un restaurant"
+      onPress={() => {
+        props.navigation.navigate("Result");
+        // searchResto();
+        props.saveSearchResto(searchAddressResto, dateInfos, time, filter)
 
-                      return(
-                        listFilter(i, equipement.name)
-                      )
-                    })}
-                  </View>
-                </View>
-              </View>
-              )
-            }
-          })
-        }
+      }}
+    />
 
-        {/* EQUIPEMENTS FILTERS */}
-        {
-          filters && filters.map((data) => {
-            if(data.id == 4) {
-              return(
-              <View key = {data.id}>
-                <View style={styles.filtreContainer}>
-                  {/* HEADER */}
-                  <Text style={styles.title}>{data.categoryName}</Text>
-                  {/* FILTERS */}
-                  <View style={styles.filtreCarre}>
-                    {data.filtres && data.filtres.map((equipement, i) => {
+  </ScrollView>
 
-                      return(
-                        listFilter(i, equipement.name)
-                        
-                      )
-                    })}
-                  </View>
-                </View>
-              </View>
-              )
-            }
-          })
-        }
-
-        {/* ACCESS FILTERS */}
-        {
-          filters && filters.map((data) => {
-            if(data.id == 5){
-              return(
-                <View key = {data.id}>
-                  <View style={styles.filtreContainer}>
-                    {/* HEADER */}
-                    <Text style={styles.title}>{data.categoryName}</Text>
-                    {/* FILTERS */}
-                    <View style={styles.filtreCarre}>
-                      {data.filtres && data.filtres.map((accessibilite, i) => {
-                        return(
-                          listFilter(i, accessibilite.name)
-                        )
-                      })}
-                    </View>
-                  </View>
-                </View>
-                )
-            }
-          })
-        }
-
-      {/* BUTTON SEARCH */}
-      <Button
-        style={{ justifyContent: "flex-end" }}
-        title="Rechercher un restaurant"
-        onPress={() => {
-          props.navigation.navigate("Result");
-          // searchResto();
-          props.saveSearchResto(searchAddressResto, dateInfos, time.getHours() + ':' + time.getMinutes(), filter)
-
-        }}
-      />
-
-    </ScrollView>
   );
 }
 
@@ -410,7 +386,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     borderRadius: 10,
-  }
+  },
 });
 
 function mapDispatchToProps(dispatch) {
